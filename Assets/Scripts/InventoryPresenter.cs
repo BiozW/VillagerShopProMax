5424061b42ff10d4cd458690affcdcac40e9b104
BiozW <jirayusuk@gmail.com>
2023-10-06 19:38:34 +0700

    EDIT: Inventory presenter to old script before conflict

--- Assets/Scripts/InventoryPresenter.cs
+++ Assets/Scripts/InventoryPresenter.cs
@@ -1,11 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Collections;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Networking;
-using Newtonsoft.Json;
-
 
 namespace Minecraft.InventorySystem
 {
@@ -20,18 +15,12 @@ namespace Minecraft.InventorySystem
         [SerializeField] UIInventory ui;
         [SerializeField] Inventory inventory;
         [SerializeField] int pageSize;
-        [SerializeField] string urlLoadPath;
         
+        //This list tells the UI what name and icon to set for each category.
         [SerializeField] List<CategoryInfo> categoryInfoList = new List<CategoryInfo>();
-        [SerializeField] GameObject loadingScene;
-        [SerializeField] TMP_Text loadText;
         public PurchaseManager purchaseManager;
         public ShopAnimation shopAnimation;
 
-        private void Awake()
-        {
-            LoadScoreFromGoogleDrive();
-        }
         void Start()
         {
             RefreshUI();
@@ -164,39 +153,6 @@ namespace Minecraft.InventorySystem
             
             //Draw the results! Convert to array to prevent the results from being changed accidentally.
             ui.SetItemList(uiDataList.ToArray());
-        }
-        IEnumerator LoadScoreRoutine(string url)
-        {
-            UnityWebRequest webRequest = UnityWebRequest.Get(url);
-            DownloadHandler handle = webRequest.downloadHandler;
-            loadingScene.SetActive(true);
-            webRequest.SendWebRequest();
-            while (!webRequest.isDone)
-            {
-                loadText.text = "Loading: " + (int)(webRequest.downloadProgress * 100f) + "%";
-                Debug.Log("0");
-                yield return null;
-            }
-            loadingScene.SetActive(false);
-
-            if (webRequest.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError(webRequest.error);
-            }
-            else
-            {
-                var downloadedText = webRequest.downloadHandler.text;
-                Debug.Log("Receive Data : " + downloadedText);
-                inventory.itemList = JsonConvert.DeserializeObject<List<ItemData>>(downloadedText);
-            }
-            
-            RefreshUI();
-        }
-
-        [ContextMenu(nameof(LoadScoreFromGoogleDrive))]
-        void LoadScoreFromGoogleDrive()
-        {
-            StartCoroutine(LoadScoreRoutine(urlLoadPath));
         }
     }
 }
